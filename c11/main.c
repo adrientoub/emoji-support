fc12c0ed19bb36def2fed6ced2a0af811c3283de
Adrien Toubiana <toubia_a@epita.fr>
2015-07-29 13:21:02 +0200

    Add C11 code example

---
+++ c11/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct 😅
+{
+  int 😃;
+} 😋;
+
+void 😂()
+{
+  😋 😄 ;
+  😄.😃 = 100;
+
+  printf("😄.😃 = %d\n", 😄.😃);
+}
+
+int main()
+{
+  int 😀 = 0;
+  printf("😀 = %d\n", 😀);
+  😂();
+  return 0;
+}
