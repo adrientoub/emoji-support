a79dae121c7ffa0567e1778e364668caf31d4c8d
Adrien Toubiana <toubia_a@epita.fr>
2015-07-29 17:46:04 +0200

    Add Java code using emojis

---
+++ java/Emoji.java
@@ -0,0 +1,13 @@
+/**
+ * Created by adrien on 29/07/15.
+ */
+public class Emoji
+{
+  public static void main(String[] args)
+  {
+    String s = "?";
+    char c = '?';
+    System.out.println(s);
+    System.out.println(c);
+  }
+}
