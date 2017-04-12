8e182438216ced04a38ddfb9c56f5714694be4c0
Adrien Toubiana <adrientoub@hotmail.com>
2017-04-12 20:58:28 +0200

    Added Scala code example

---
+++ scala/main.scala
@@ -0,0 +1,19 @@
+object `👨` {
+  def main(args: Array[String]): Unit = {
+    val `👌` = "👌"
+    println(`👌`)
+    println("Hello world!")
+    `😄`()
+  }
+
+  def `😄`() {
+    val `😎` = new `😇`()
+    `😎`.`😛`()
+  }
+}
+
+class `😇`() {
+  def `😛`() {
+    println("😉")
+  }
+}
